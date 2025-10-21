0dae8c64a2e35f1d8fd6beaf28fca9c67d971223
Christos Themelis <themelisx@gmail.com>
2025-10-21 23:43:18 +0300

    UI design files

---
+++ UI design/buttons_events.py
@@ -0,0 +1,28 @@
+
+def onControl1Click(event_struct):
+    return
+
+
+def onControl2Click(event_struct):
+    return
+
+
+def onControl3Click(event_struct):
+    return
+
+
+def onControl4Click(event_struct):
+    return
+
+
+def onControl5Click(event_struct):
+    return
+
+
+def onControl6Click(event_struct):
+    return
+
+
+def onControl7Click(event_struct):
+    return
+
